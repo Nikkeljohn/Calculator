aebb29f8c5475ddc8d690b9361b55d864fa6584f
Nikkeljohn <nikkeljohn120@gmail.com>
2022-10-30 17:04:15 +0000

    added event listener and h so we can se the innetext and he button clicked , used switch and used default so we can enter the same value of innertext of button and display

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,5 +1,24 @@
+// two variable created for display and button function 
+
 let display = document.getElementById('display');
 
 let buttons = Array.from(document.getElementsByClassName('button'));
 
-buttons.map()
+//we map through our buttons array and add click eventlistener for every button in our array
+buttons.map(button =>{
+    //add a call back function for our eventlistener by h which will get event
+    button.addEventListener('click',(h) =>{
+        // we use switch statement and can inseert the same value typed 
+        //so by deafult will add innertext of button to inner text of display
+        switch(h.target.innertext){
+            
+        }
+       /* we use to identify which button is clicked
+        console.log('clicked')
+        console.log(h);
+        console.log(h.target);
+        console.log(h.target.innerText);*/
+
+
+    });
+});
