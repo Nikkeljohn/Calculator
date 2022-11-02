c66f6e08dfc01ed954fdb44394a603ddf56a8750
Nikkeljohn <nikkeljohn120@gmail.com>
2022-11-02 10:06:28 +0000

    adde new button and done few changes in header and footer

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -6,23 +6,27 @@ let buttons = Array.from(document.getElementsByClassName('button'));
 
 //we map through our buttons array and add click eventlistener for every button in our array
 buttons.map(button =>{
-    //add a call back function for our eventlistener by h which will get event
+    //add a call back function for our eventlistener 
     button.addEventListener('click',(h) =>{
         // we use switch statement and can inseert the same value typed 
-        //so by deafult will add innertext of button to inner text of display
+        
         switch(h.target.innerText){
            
-           //and in this case if user clicks the "c" button it clears the display by setting the innertext an empty string
+           //a
 
             case 'C':         
                 display.innerText = '';
                 //we break the statement by executing break and doesn't show c
                 break;
 
+                //
+
                 case '=':
 
-
-        }
+                //will display content when clicked
+                    default:
+                        display.innerText += h.target.innerText;
+ }
         
         
        // tried  to identify which button is clicked is working 
