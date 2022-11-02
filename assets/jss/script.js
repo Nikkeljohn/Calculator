51867e9bac7711aa7aed4589f1fd22c58bc18dbe
Nikkeljohn <nikkeljohn120@gmail.com>
2022-11-02 14:49:18 +0000

    added few function like back arrow so user can delete the number and = with eval function so it can take string as a parameter and excute it as java script

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -4,32 +4,46 @@ let display = document.getElementById('display');
 
 let buttons = Array.from(document.getElementsByClassName('button'));
 
-//we map through our buttons array and add click eventlistener for every button in our array
-buttons.map(button =>{
+//we map through our buttons array and add click eventlistener 
+buttons.map(button => {
     //add a call back function for our eventlistener 
-    button.addEventListener('click',(h) =>{
+    button.addEventListener('click', (h) => {
         // we use switch statement and can inseert the same value typed 
-        
-        switch(h.target.innerText){
-           
-           //a
 
-            case 'C':         
+        switch (h.target.innerText) {
+
+            //a
+
+            case 'C':
                 display.innerText = '';
                 //we break the statement by executing break and doesn't show c
                 break;
 
-                //
+            // here we add back arrow to delete miss typed number
 
-                case '=':
+            case '←':
+                // this will only remove if any test is on display
+                if (display.innerText) {
+                    // here we use string slice method to remove from display string
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                // break is used to aviod arrow button
+                break;
 
-                //will display content when clicked
-                    default:
-                        display.innerText += h.target.innerText;
- }
-        
-        
-       // tried  to identify which button is clicked is working 
+                //most immportant function "="
+                case "=":
+                    //  used eval function so it can take string and excute as js
+                    display.innerText = eval(display.innerText);
+                    //used break function to aviod display =
+                    break;
+
+            //will display content when clicked
+            default:
+                display.innerText += h.target.innerText;
+        }
+
+
+        // tried  to identify which button is clicked is working 
 
         console.log('clicked')
         console.log(h);
@@ -49,18 +63,18 @@ let btn = document.getElementById("myBtn");
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btn.onclick = function () {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
