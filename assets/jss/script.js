4a1ac0d09b96715924be14539889df13564a1cb9
Nikkeljohn <nikkeljohn120@gmail.com>
2022-11-08 10:24:12 +0000

    done media query for calculator

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -5,9 +5,13 @@ let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('button'));
 
 //we map through our buttons array and add click eventlistener 
+
 buttons.map(button => {
+
     //add a call back function for our eventlistener 
+
     button.addEventListener('click', (h) => {
+
         // we use switch statement and can inseert the same value typed 
 
         switch (h.target.innerText) {
@@ -16,35 +20,48 @@ buttons.map(button => {
 
             case 'C':
                 display.innerText = '';
+
                 //we break the statement by executing break and doesn't show c
+
                 break;
 
             // here we add back arrow to delete miss typed number
 
             case '←':
+
                 // this will only remove if any test is on display
+                
                 if (display.innerText) {
                     // here we use string slice method to remove from display string
+
                     display.innerText = display.innerText.slice(0, -1);
                 }
                 // break is used to aviod arrow button
+
                 break;
 
             //most immportant function "="
+
             case "=":
+
                 // used to handle in valid expression
+
                 try {
                     //  used eval function so it can take string and excute as js
+
                     display.innerText = eval(display.innerText);
                 }
                 //if invalid expression is done then it will excute whats in catch statement
+
                 catch {
                     display.innerText = "ERROR!";
                 }
                 //used break function to aviod display =
+
                 break;
 
             //will display content when clicked
+
             default:
                 display.innerText += h.target.innerText;
         }
@@ -52,15 +69,19 @@ buttons.map(button => {
     });
 });
 // Get the modal
+
 let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
+
 let btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
+
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
+
 btn.onclick = function () {
     modal.style.display = "block";
 }
